3d9fc3410a2c7057f9eb5a05c68ff1d429b50fc8
Carlos Vargas <csv17@live.unc.edu>
2018-09-23 13:42:07 -0400

    added index and js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//when the user clicks the button,
+//a different message appears in a box
+//depending on the day of the week
+
+//Objects: the button, the message, and the box
+//Methods: add the message to the empty box
+//Events: the button click
+
+//Get the button and the text box from the HTML
+var btn = document.getElementById('date-btn');
+var box = document.getElementById('date-text');
+//initialize a variable to hold the new HTML content
+var msg;
+//now you'll need to get the day of the week, via JS...
+
+
+btn.addEventListener('click', function(){
+  console.log('testing the click event');
+  //code goes here//
+  
+  
+});
+
