b0448d414e1138e3618a7e624a678c6e48985b7c
Carlos Vargas <csv17@live.unc.edu>
2018-09-23 15:53:59 -0400

    date now showing in DOM

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ function showDate(){
     var d = new Date();
     var days = ["Sunday", "Monday", "Tuesday", "Tuesday", "Thursday", "Friday", "Saturday"];
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    
-    var date = " 'Today is Monday, September 17, 2018.'";
 
-    console.log('Today is ' + days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear() + ".");
+    var date = 'Today is ' + days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear() + ".";
+
+    document.getElementById('date-text').innerHTML = date;
 
 };
 
